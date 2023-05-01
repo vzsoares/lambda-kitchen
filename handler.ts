e2f8a86fc1ea955d2bee3775f46cc7eab6bcd9df
vzsoares <jogolots2@gmail.com>
2023-04-30 21:54:53 -0300

    config prettier

--- handler.ts
+++ handler.ts
@@ -1,9 +1,9 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const handler = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
   const queries = JSON.stringify(event.queryStringParameters);
   return {
     statusCode: 200,
-    body: JSON.stringify({ queries: `${queries}`, hi: "hello" }),
+    body: JSON.stringify({ queries: `${queries}`, hi: 'hello' }),
   };
 };
