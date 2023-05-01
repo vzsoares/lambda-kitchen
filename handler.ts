2c28b5db11a19cec973f14ce20e173aa81cb04b9
vzsoares <jogolots2@gmail.com>
2023-04-30 21:39:49 -0300

    basic eslint config

--- handler.ts
+++ handler.ts
@@ -1,11 +1,9 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const handler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
+export const handler = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
   const queries = JSON.stringify(event.queryStringParameters);
   return {
     statusCode: 200,
     body: JSON.stringify({ queries: `${queries}`, hi: "hello" }),
   };
-}; 
+};
