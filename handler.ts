27c3a152b9890774de1fce65d897b987268d969e
vzsoares <jogolots2@gmail.com>
2023-05-02 20:23:53 -0300

    zip lambda

--- handler.ts
+++ handler.ts
@@ -1,6 +1,8 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const handler = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
+export const handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
   const queries = JSON.stringify(event.queryStringParameters);
   return {
     statusCode: 200,
