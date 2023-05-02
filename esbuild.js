2ceeab2ed44790c830a3416250a98bb81cbdaff8
vzsoares <jogolots2@gmail.com>
2023-05-02 20:36:07 -0300

    add invoke scripts

--- esbuild.js
+++ esbuild.js
@@ -1,11 +1,11 @@
-const esbuild = require("esbuild");
+const esbuild = require('esbuild');
 
 esbuild.build({
-  entryPoints: ["handler.ts"],
-  platform: "node",
+  entryPoints: ['handler.ts'],
+  platform: 'node',
   bundle: true,
   minify: true,
   sourcemap: true,
-  target: "es2020",
-  outfile: "dist/index.js",
+  target: 'es2020',
+  outfile: 'dist/index.js',
 });
