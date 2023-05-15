b52d69fbec6ed01816d25770af88b7e941713b62
vzsoares <jogolots2@gmail.com>
2023-05-15 20:34:37 -0300

    enh script

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,25 @@
 set -e 
 set -o pipefail
 
-[ -e function.zip ] && rm -f function.zip
-
-[ -e dist ] && rm -f -r dist
+LAYER_NAME="helpers"
+FINAL_PATH="./lib/nodejs/$LAYER_NAME"
 
 STAGE=$1
 AWS_PROFILE=$2
 
-test -n "$STAGE" || \
+test -n "$STAGE" && test -n "$AWS_PROFILE" || \
 (echo "No essential arg variables provided" && false)
 
-LAYER_NAME="helpers"
-FINAL_PATH="./lib/nodejs/$LAYER_NAME"
+[ -e function.zip ] && rm -f function.zip
+
+[ -e dist ] && rm -f -r dist
 
 node ./esbuild.js
 
 mkdir -p "$FINAL_PATH" && cp -r dist/* "$FINAL_PATH" && \
 (cd "$FINAL_PATH" && npm init -y 1> /dev/null)
 
-# sls deploy --stage $STAGE --aws-profile $AWS_PROFILE
+# npm sls deploy --stage $STAGE --aws-profile $AWS_PROFILE
 
 echo "SUCCESS"
 
