c0c92f258dcb87bb86299b61d548b4dbf98c1a47
vzsoares <jogolots2@gmail.com>
2023-05-15 23:25:33 -0300

    feat zod error middleware

--- handler.ts
+++ handler.ts
@@ -1,11 +1,29 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
+import * as z from 'zod';
+
+import sayHello from './actions/sayHello';
+
 import helpers from '/opt/nodejs/lambda-layer/';
 
-export const handler = async (
+const handler = async (
   event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
-  const queries = JSON.stringify(event.queryStringParameters);
+  fn: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>,
+) => {
+  try {
+    const result = await fn(event);
 
-  return helpers.response(200, { hi: 'hello', queries: `${queries}` });
+    return result;
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof z.ZodError) {
+      return helpers.response(400, null, error.issues);
+    }
+
+    return helpers.response(500, null, ['Internal server error']);
+  }
 };
+
+export const sayHelloHandler = (e: APIGatewayProxyEvent) =>
+  handler(e, sayHello);
